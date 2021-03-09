384ef2450c2bd66ef45fbffb65dfb2c1f59946e8
Sulav Khadka <sulav.khadka787@gmail.com>
2021-03-09 07:54:05 -0700

    Count increase decrease reset

---
+++ 04Counter/app.js
@@ -0,0 +1,34 @@
+
+let count =0;
+
+const value=document.querySelector('#value');
+const btns=document.querySelectorAll(".btn");
+
+console.log(btns);
+
+btns.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        //console.log(e.currentTarget);
+        const styles=e.currentTarget.classList;
+        //console.log(styles);
+        if(styles.contains('decrease')){
+            count--;
+        }else if(styles.contains('increase')){
+            count++;
+        }else{
+            if(styles.contains('reset')){
+                count=0;
+            }
+        }
+        if(count>0){
+            value.style.color="green";
+        }
+        if(count<0){
+            value.style.color="red";
+        }
+        if(count === 0){
+            value.style.color="#222";
+        }
+        value.textContent=count;
+    })
+});
